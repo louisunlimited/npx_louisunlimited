08df3556d9e1a0bed8f2db0f875ad6253e024ac2
Louis Qian <yuq4@illinois.edu>
2024-04-30 21:11:07 -0500

    chore!: renamed `bin.js` to `cli.js`

---
+++ cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log(`
+.------------------.
+|  Hi, I'm Louis   |
+'------------------'
+        ^      (\\_/)
+        '----- (O.0)
+               (> <)
+`);
+
+console.log("Nice seeing you!");
